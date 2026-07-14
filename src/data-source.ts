11026b85fc8d11e2c84ad22a047474e9ebaab801
alaaateff <alaaatef3200@gmail.com>
2026-07-14 13:28:09 +0300

    modified the app module to include migrate and added data-source file

---
+++ src/data-source.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+
+import { DataSource } from 'typeorm';
+
+
+export default new DataSource({
+
+  type:'postgres',
+
+  host:process.env.DB_HOST,
+
+  port:parseInt(
+    process.env.DB_PORT ?? '5432',
+    10
+  ),
+
+  username:process.env.DB_USER,
+
+  password:process.env.DB_PASSWORD,
+
+  database:process.env.DB_NAME,
+
+
+  entities:[
+    'src/modules/**/*.entity.ts',
+  ],
+
+
+  migrations:[
+    'src/migrations/*.ts',
+  ],
+
+});
