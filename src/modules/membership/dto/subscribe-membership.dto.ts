eec78018e3ef27ca447be035075ecdd2825a351c
alaaateff <alaaatef3200@gmail.com>
2026-07-21 19:15:53 +0300

    removed customerId from membership-dto

--- src/modules/membership/dto/subscribe-membership.dto.ts
+++ src/modules/membership/dto/subscribe-membership.dto.ts
@@ -1,9 +1,8 @@
-import { IsInt, IsString } from 'class-validator';
+import { IsInt } from "class-validator";
 
 export class SubscribeMembershipDto {
-  @IsString()
-  customerId: string;
 
   @IsInt()
   planId: number;
+
 }
