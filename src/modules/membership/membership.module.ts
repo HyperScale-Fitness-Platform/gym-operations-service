bd5cff1420c424844a8ca088b8be39c3f9532f1d
alaaateff <alaaatef3200@gmail.com>
2026-08-15 04:12:55 +0300

    modified membership module

--- src/modules/membership/membership.module.ts
+++ src/modules/membership/membership.module.ts
@@ -10,11 +10,12 @@ import {MembershipExpirationJob} from './jobs/membership-expiration.job';
 import { MembershipBenefit } from './entities/membership-benefit.entity';
 import { CustomerBenefit } from './entities/customer-benefit.entity';
 import { PtPackage } from './entities/pt-package.entity';
+import { EventPublisher } from 'src/events/publishers';
 
 @Module({
   imports:[TypeOrmModule.forFeature([MembershipPlan,Membership,MembershipFreeze,MembershipBenefit,CustomerBenefit, PtPackage])],
   controllers:[MembershipController],
-  providers: [MembershipService,MembershipFreezeJob,MembershipExpirationJob],
-  exports: [MembershipService],
+  providers: [MembershipService,MembershipFreezeJob,MembershipExpirationJob,EventPublisher],
+  exports: [MembershipService,EventPublisher],
 })
 export class MembershipModule {}
