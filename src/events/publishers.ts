95805370cd8634123eba9938fb2eb0511cf3f123
alaaateff <alaaatef3200@gmail.com>
2026-08-15 04:10:03 +0300

    added publishers.ts file

--- src/events/publishers.ts
+++ src/events/publishers.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { kafkaProducer } from './kafka';
 
 @Injectable()
-export class EventPublisher {
-  async publish(eventName: string, payload: Record<string, unknown>) {
-    console.log(`[event] ${eventName}`, payload);
+export class EventPublisher implements OnModuleInit, OnModuleDestroy {
+
+  async onModuleInit() {
+    await kafkaProducer.connect();
+    console.log('Gym Operations Kafka Producer connected');
+  }
+
+  async publish(
+    eventName: string,
+    payload: Record<string, unknown>,
+  ) {
+    await kafkaProducer.send({
+      topic: eventName,
+      messages: [
+        {
+          key: String(payload.bookingId ?? ''),
+          value: JSON.stringify(payload),
+        },
+      ],
+    });
+
+    console.log(`[Kafka] Published ${eventName}`, payload);
+  }
+
+  async onModuleDestroy() {
+    await kafkaProducer.disconnect();
   }
 }
